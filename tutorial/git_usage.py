9e8eb6fc54e35701f43481512ec284fbcfb279ed
Tibor Makai <tbee.babylove@gmail.com>
2023-12-21 19:26:05 +0100

    initial commit

---
+++ tutorial/git_usage.py
@@ -0,0 +1,14 @@
+"""
+Git kezelés:
+Eldönteni h melyik fileokat / mappákat NEM akarod feltölteni a Git repositoryba
+
+Git Flow:
+Létrehozol vmilyen változást a kódbázison
+-> új sor / karakter / file / mappa
+-> ezeket a válzotásokat figyelni akarod: git add.
+    - azt az állapotot, amit figyelek, ahhoz mérten fogom tudni a visszaállítást alkalmazni
+    - vagy a flow további lépcsőjére lépni
+
+    uj changes
+
+"""
